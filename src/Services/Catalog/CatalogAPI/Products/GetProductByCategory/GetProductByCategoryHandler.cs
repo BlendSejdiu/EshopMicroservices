f5e66694df35fe81b27cda79410f135f81a2c656
BlendSejdiu <119117327+BlendSejdiu@users.noreply.github.com>
2025-08-16 12:56:44 +0200

    Added GetByCategory - CQRS

---
+++ src/Services/Catalog/CatalogAPI/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,24 @@
+﻿
+using CatalogAPI.Products.GetProductById;
+
+namespace CatalogAPI.Products.GetProductsByCategory
+{
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+    internal class GetProductByCategoryQueryHandler(IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger) : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByCategoryQueryHandler.Handle called with {@Query}", query);
+
+            var products = await session.Query<Product>().Where(x => x.Category.Contains(query.Category)).ToListAsync(cancellationToken);
+
+            if (products is null)
+            {
+                throw new ProductNotFoundException();
+            }
+
+            return new GetProductByCategoryResult(products);
+        }
+    }
+}
