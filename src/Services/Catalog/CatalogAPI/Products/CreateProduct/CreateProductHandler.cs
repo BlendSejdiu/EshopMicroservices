39e6ce76fa579fd082cb77d677cf8543b80a4810
BlendSejdiu <119117327+BlendSejdiu@users.noreply.github.com>
2025-08-16 12:56:26 +0200

    Added CreateProduct - CQRS

---
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,39 @@
+﻿namespace CatalogAPI.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+
+    public class CreateProductValidator : AbstractValidator<CreateProductCommand> 
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Price must be greater than 0");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("Must have a image");
+        }
+    }
+    internal class CreateProductCommandHandler(IDocumentSession session, ILogger<CreateProductCommandHandler> logger) : ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("CreateProductCommand.Handle called with {@Command}", command);
+
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            session.Store(product);
+            await session.SaveChangesAsync(cancellationToken);
+
+            return new CreateProductResult(product.Id);
+
+        }
+    }
+}
