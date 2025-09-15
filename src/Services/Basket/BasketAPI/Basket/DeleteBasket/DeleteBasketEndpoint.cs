24204118ebdc59fd681f87a43524e56431dec15d
BlendSejdiu <119117327+BlendSejdiu@users.noreply.github.com>
2025-09-15 12:33:32 +0200

    DeleteBasket endpoint and handler

---
+++ src/Services/Basket/BasketAPI/Basket/DeleteBasket/DeleteBasketEndpoint.cs
@@ -0,0 +1,23 @@
+﻿
+using BasketAPI.Basket.StoreBasket;
+
+namespace BasketAPI.Basket.DeleteBasket
+{
+    //public record DeleteBasketRequest(string UserName);
+    public record DeleteBasketResponse(bool IsSuccess);
+    public class DeleteBasketEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/basket/{userName}", async (string userName, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteBasketCommand(userName));
+
+                var response = result.Adapt<DeleteBasketResponse>();
+
+                return Results.Ok(response);
+            }).WithName("DeleteUserBasket").Produces<DeleteBasketResponse>(StatusCodes.Status200OK)
+              .ProducesProblem(StatusCodes.Status400BadRequest).WithSummary("Delete User Basket").WithDescription("Delete User Basket");
+        }
+    }
+}
