8b25f6a24a0e056a92e2ecda30c004d118f51cf7
BlendSejdiu <119117327+BlendSejdiu@users.noreply.github.com>
2025-12-01 11:09:33 +0100

    Fixed connection string for Development purposes

--- src/Services/Ordering2/OrderingAPI/DependencyInjection.cs
+++ src/Services/Ordering2/OrderingAPI/DependencyInjection.cs
@@ -1,4 +1,4 @@
-﻿using Carter;
+﻿using BuildingBlocks.Exceptions.Handler;
 
 namespace OrderingAPI
 {
@@ -7,12 +7,16 @@ namespace OrderingAPI
         public static IServiceCollection AddAPIServices(this IServiceCollection services)
         {
             services.AddCarter();
+            services.AddExceptionHandler<CustomExceptionHandler>();
             return services;
         }
 
         public static WebApplication UseAPIServices(this WebApplication app)
         { 
             app.MapCarter();
+
+            app.UseExceptionHandler(options => { });
+
             return app;
         }
     }
