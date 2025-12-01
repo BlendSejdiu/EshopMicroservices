2bf0e884fd3ece33a1b9b17b43214ec025eaa09e
BlendSejdiu <119117327+BlendSejdiu@users.noreply.github.com>
2025-12-01 09:30:40 +0100

    GetOrdersByName query and handler

---
+++ src/Services/Ordering2/Ordering.Application/Orders/Queries/GetOrderByName/GetOrdersByNameHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrderByName;
+
+public class GetOrdersByNameHandler(IApplicationDbContext db) : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResult>
+{
+    public async Task<GetOrdersByNameResult> Handle(GetOrdersByNameQuery query, CancellationToken cancellationToken)
+    {
+        var orders = await db.Orders.Include(o => o.OrderItems).AsNoTracking()
+                              .Where(o => o.OrderName.Value.Contains(query.Name))
+                              .OrderBy(o => o.OrderName).ToListAsync(cancellationToken);
+
+        return new GetOrdersByNameResult(orders.ToOrderDTOList());
+    }
+}
